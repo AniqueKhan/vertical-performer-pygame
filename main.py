5773641e7f9775cae6d0f886bdf92965f313a23e
Anique <aniquekhan004@gmail.com>
2023-03-31 15:26:00 +0500

    2nd video done
    2nd video done

---
+++ main.py
@@ -0,0 +1,56 @@
+# Import Libraries
+import pygame as pg
+
+# Initialize Game
+pg.init()
+
+# Screen Dimensions
+SCREEN_WIDTH, SCREEN_HEIGHT = 400, 600
+
+# Define Colors
+WHITE = (255,255,255)
+
+# Create Game Window
+screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pg.display.set_caption("Vertical Performer - Anique")
+
+# Load Images
+bg_image = pg.image.load('assets/bg.png').convert_alpha()
+performer_image =pg.transform.scale(pg.image.load('assets/performer.png').convert_alpha(),(45,45))
+
+
+# Performer Class
+class Performer():
+    def __init__(self,x,y):
+        self.image = performer_image
+        self.width , self.height = 25 , 40
+        self.rect = pg.Rect(0,0,self.width,self.height)
+        self.rect.center = (x,y)
+
+    def draw(self):
+        screen.blit(self.image,(self.rect.x-12,self.rect.y-5))
+        pg.draw.rect(screen,WHITE,self.rect,2)
+
+performer = Performer(x=SCREEN_WIDTH//2, y=SCREEN_HEIGHT-150)
+
+
+
+# Game Loop
+run = True
+while run:
+
+    # Draw Background
+    screen.blit(source=bg_image, dest=(0,0))
+
+    # Draw Performer
+    performer.draw()
+
+    # Event handler
+    for event in pg.event.get():
+        if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key==pg.K_ESCAPE):
+            run=False
+
+    # Update Display Window
+    pg.display.update()
+
+pg.quit()
